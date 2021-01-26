acd5271bd60819168d82ab5c2504ee6fc35d1c01
John <johndev4@gmail.com>
2021-01-26 18:43:56 +0800

    update

--- application/views/login.php
+++ application/views/login.php
@@ -14,7 +14,7 @@
 	<link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css'); ?>">
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
 	<!-- Theme style -->
-	<link rel="stylesheet" href="<?= base_url('assets/AdminLTE-3.1.0-rc/dist/css/AdminLTE.min.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/AdminLTE-3.1.0-rc/dist/css/adminlte.min.css') ?>">
 	<!-- icheck bootstrap -->
 	<link rel="stylesheet" href="<?= base_url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
 	<!-- Google Font: Source Sans Pro -->
